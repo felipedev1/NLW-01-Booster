569979c928fa99a40cccf948f48b313349ad8da1
Felipe <felipe.pereira.dev@gmail.com>
2020-06-03 12:19:11 -0300

    Create component page home

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import './App.css';
+import Home from './pages/Home'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello Word</h1>
-      </header>
-    </div>
+    <Home />
   );
 }
 
